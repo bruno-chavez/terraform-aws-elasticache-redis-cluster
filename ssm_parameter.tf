cb810c478dc9eaec30a8fae65d520ff4f47c95fa
bruno-chavez <bruno.chavezl@sansano.usm.cl>
2020-09-13 23:39:13 -0300

    Renamed ssm parameter

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "cache_url" {
-  name = "cache-url"
+resource "aws_ssm_parameter" "redis_cluster_url" {
+  name = "redis-cluster-url"
   type = "SecureString"
   key_id = "alias/aws/ssm"
+  overwrite = true
   value = "${aws_elasticache_replication_group.redis_cluster.configuration_endpoint_address}:${var.port}"
 }
